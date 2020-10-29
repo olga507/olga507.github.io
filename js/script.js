a39345bd37ab75a6499efece020e2cbc14e1fcf5
Olga <ola_klmta@mail.ru>
2020-10-29 15:40:53 +0300

    download

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu');
+      closeElem = document.querySelector('.menu__close');
+      menuItem = document.querySelectorAll('.menu__link');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('menu_close');
+        menu.classList.toggle('active');
+    })
+})
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    window.onclick = function(event) {
+        if(event.target.className == 'menu__overlay' && menu.classList.contains('active')) {
+            menu.classList.remove('active');
+        } 
+    }
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: 'mailer/smart.php',
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find('input').val('');
+        
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
